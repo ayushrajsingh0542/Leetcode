55d8db956db0d6645bd9c9f0689d621fbb8b89f2
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 20:05:19 +0530

    Create 180-ConsecutiveNumbers

---
+++ 180-ConsecutiveNumbers/180-ConsecutiveNumbers.sql
@@ -0,0 +1,6 @@
+-- Last updated: 15/08/2026, 20:05:19
+# Write your MySQL query statement below
+select distinct(a.num) as ConsecutiveNums
+from logs as a
+join logs as b on a.id = b.id+1 and a.num = b.num
+join logs as c on a.id = c.id+2 and a.num = c.num;
