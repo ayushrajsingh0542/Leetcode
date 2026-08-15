21d44bd9deb7956fc317f939e8d297e38fcdf00e
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 20:01:24 +0530

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.java
@@ -0,0 +1,17 @@
+// Last updated: 15/08/2026, 20:01:24
+class Solution {
+    public boolean checkInclusion(String p, String s) {
+        if (p.length() > s.length()) return false;
+        int[] f1 = new int[26], f2 = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            f1[p.charAt(i) - 'a']++;
+            f2[s.charAt(i) - 'a']++;
+        }
+        for (int i = p.length(); i < s.length(); i++) {
+            if (Arrays.equals(f1, f2)) return true;
+            f2[s.charAt(i) - 'a']++;
+            f2[s.charAt(i - p.length()) - 'a']--;
+        }
+        return Arrays.equals(f1, f2);
+    }
+}
