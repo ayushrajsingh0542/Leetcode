d62d03e0516353da6bfaa11b5e0d3343563bedbb
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 19:56:02 +0530

    Create 1319-UniqueNumberOfOccurrences

---
+++ 1319-UniqueNumberOfOccurrences/1319-UniqueNumberOfOccurrences.java
@@ -0,0 +1,19 @@
+// Last updated: 15/08/2026, 19:56:02
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+
+        for(int i=0; i<arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+            set1.add(arr[i]);
+        }
+
+        for(int i : map.keySet()){
+            set2.add(map.get(i));
+        }
+
+        return set1.size()==set2.size();
+    }
+}
