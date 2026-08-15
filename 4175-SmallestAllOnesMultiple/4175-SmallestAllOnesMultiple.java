da105cada1c0bba78993443f02e8312106aeb613
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 19:47:47 +0530

    Create 4175-SmallestAllOnesMultiple

---
+++ 4175-SmallestAllOnesMultiple/4175-SmallestAllOnesMultiple.java
@@ -0,0 +1,16 @@
+// Last updated: 15/08/2026, 19:47:47
+class Solution {
+    public int minAllOneMultiple(int k) {
+         if(k%2==0 || k%5==0) return -1;
+
+        int remain = 0;
+
+        for(int i=1;i<=k;i++)
+            {
+                remain = (remain*10+1)%k;
+                if(remain ==0) return i;
+            }
+
+        return -1;
+    }
+}
