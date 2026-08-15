7b586f62963a2482bc3d251e16b4e1bcf67a7423
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 20:00:34 +0530

    Create 620-NotBoringMovies

---
+++ 620-NotBoringMovies/620-NotBoringMovies.sql
@@ -0,0 +1,6 @@
+-- Last updated: 15/08/2026, 20:00:34
+# Write your MySQL query statement below
+select id ,movie,description,rating
+from Cinema 
+where id%2!=0 and description!='boring'
+order by rating desc
