ea19509963a5197154c50f247c87be6d88d5003a
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 19:57:04 +0530

    Create 1155-ProductSalesAnalysisIii

---
+++ 1155-ProductSalesAnalysisIii/1155-ProductSalesAnalysisIii.sql
@@ -0,0 +1,9 @@
+-- Last updated: 15/08/2026, 19:57:04
+# Write your MySQL query statement below
+SELECT product_id, year as first_year, quantity,price
+FROM Sales
+WHERE (product_id,year) in (
+SELECT product_id,MIN(year)
+FROM Sales
+GROUP BY product_id
+)
