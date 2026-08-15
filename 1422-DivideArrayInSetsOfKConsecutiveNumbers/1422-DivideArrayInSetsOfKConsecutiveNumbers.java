eefa4e8674671991ee6773e59d25fdedb458a451
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 19:55:33 +0530

    Create 1422-DivideArrayInSetsOfKConsecutiveNumbers

---
+++ 1422-DivideArrayInSetsOfKConsecutiveNumbers/1422-DivideArrayInSetsOfKConsecutiveNumbers.java
@@ -0,0 +1,48 @@
+// Last updated: 15/08/2026, 19:55:33
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+       
+        if(hand.length%groupSize!=0)
+        return false;
+        Arrays.sort(hand);
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<hand.length;i++)
+        {
+            map.put(hand[i],map.getOrDefault(hand[i],0)+1);
+        }
+        int temp=0;
+        for(int i=0;i<hand.length;i++)
+        {
+            temp=0;
+            if(!map.containsKey(hand[i]))
+            {
+                continue;
+            }
+            int x=hand[i];
+            while(temp!=groupSize)
+            {
+                if(map.containsKey(x))
+                {
+                    if(map.get(x)==1)
+                    {
+                        map.remove(x);
+                    }
+                    else
+                    {
+                        map.put(x,map.get(x)-1);
+                    }
+                    x++;
+                    temp++;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+}
+        
+    
