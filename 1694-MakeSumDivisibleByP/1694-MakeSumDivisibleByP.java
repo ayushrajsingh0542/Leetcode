d314d73642a02291b9f0ba42d6eaf92443211d54
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 19:54:13 +0530

    Create 1694-MakeSumDivisibleByP

---
+++ 1694-MakeSumDivisibleByP/1694-MakeSumDivisibleByP.java
@@ -0,0 +1,25 @@
+// Last updated: 15/08/2026, 19:54:13
+class Solution {
+    public int minSubarray(int[] nums, int p) {
+      int n  = nums.length;
+      int ans = 0;
+      for(int i=0;i<n;i++){
+          ans = (ans+nums[i])%p;
+      }   
+      if( ans == 0) return 0;
+      int res =n;
+      int sum =0;
+      HashMap<Integer, Integer> map = new HashMap<>();
+      map.put(0,-1);
+      for(int i=0;i<n;i++){
+          sum = (sum+ nums[i]) %p;
+         
+          int mod = ( sum-ans+p) %p;
+           map.put(sum,i);
+          if(map.containsKey(mod)){
+              res = Math.min( res, i-map.get(mod));
+          }
+      }
+      return res < n  ? res: -1;
+    }
+}
