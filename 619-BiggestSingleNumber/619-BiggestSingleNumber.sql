d54996cf651dd899620c5e07d510aad3ec1c7552
AYUSH RAJ SINGH <105589428+ayushrajsingh0542@users.noreply.github.com>
2026-08-15 20:00:52 +0530

    Create 619-BiggestSingleNumber

---
+++ 619-BiggestSingleNumber/619-BiggestSingleNumber.sql
@@ -0,0 +1,9 @@
+-- Last updated: 15/08/2026, 20:00:52
+SELECT IFNULL((
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+    ORDER BY num DESC
+    LIMIT 1
+), NULL) AS num;
